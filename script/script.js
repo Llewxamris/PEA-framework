eee03dbb3582a2c74ae5115b25f1b8736a5e89f7
Maxwell Haley <maxwell.haley@openmailbox.org>
2017-08-28 12:17:42 -0400

    Add functionality to auto-generate titles/subtitles

--- script/script.js
+++ script/script.js
@@ -28,45 +28,65 @@ const peekaboo = (id) => {
     }
 };
 
+const iseeyou = (title, subtitle) => {
+    const t = document.getElementById('hTitle');
+    const s = document.getElementById('sTitle');
+
+    t.innerText = title;
+    s.innerText = subtitle;
+};
+
 setInterval(() => {
     // I blame Richard Chan for having to make this code chunk
     if (prevHash !== location.hash) {
         switch (location.hash) {
         case '#process':
             peekaboo('process');
+            iseeyou('Process', 'Fun Subtitle!');
             break;
         case '#teamEnvironment':
             peekaboo('teamEnvironment');
+            iseeyou('Team Environment', 'Fun Subtitle!');
             break;
         case '#projectManagement':
-            peekaboo('projectManagement');
+            peProcessekaboo('projectManagement');
+            iseeyou('Project Management', 'Fun Subtitle!');
             break;
         case '#requirements':
             peekaboo('requirements');
+            iseeyou('Requirements', 'Fun Subtitle!');
             break;
         case '#analysis':
             peekaboo('analysis');
+            iseeyou('Analysis', 'Fun Subtitle!');
             break;
         case '#design':
             peekaboo('design') ;
+            iseeyou('Design', 'Fun Subtitle!');
             break;
         case '#implementation':
             peekaboo('implementation');
+            iseeyou('Implementation', 'Fun Subtitle!');
             break;
         case '#test':
             peekaboo('test');
+            iseeyou('Test', 'Fun Subtitle!');
             break;
         case '#deployment':
             peekaboo('deployment');
+            iseeyou('Deployment', 'Fun Subtitle!');
             break;
         case '#training':
             peekaboo('training');
+            iseeyou('Training', 'Fun Subtitle!');
             break;
         case '#maintenance':
             peekaboo('maintenance');
+            iseeyou('Maintenance', 'Fun Subtitle!');
             break;
         case '#communications':
             peekaboo('communications');
+            iseeyou('Communications', 'Fun Subtitle!');
             break;
         default:
         }
