f800cffdfb82ec076c511a3061031d7b45f8c2d2
Maxwell Haley <maxwell.haley@openmailbox.org>
2018-02-20 17:01:23 -0500

    Auto-generate title and page header

--- js/generate-pages.js
+++ js/generate-pages.js
@@ -6,6 +6,9 @@ const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 const ARGV = require('minimist')(process.argv.slice(2));
 
+function capitalizeWord(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
 
 (() => {
     const masterHtml = Fs.readFileSync('../master.html', {encoding: 'utf-8'});
@@ -15,6 +18,24 @@ const ARGV = require('minimist')(process.argv.slice(2));
         const masterDom = new JSDOM(masterHtml).window.document;
         const htmlContent = Fs.readFileSync(path, {encoding: 'utf-8'});
         const masterContent = masterDom.querySelector('#content');
+        const masterTitle = masterDom.querySelector('#hTitle');
+
+        // TODO Remove hardcoded information
+        switch (filename) {
+        case 'teamEnvironment':
+            masterDom.title = '2017/2018 PEA - Team Environment';
+            masterTitle.innerHTML = 'Team Environment';
+            break;
+        case 'projectManagement':
+            masterDom.title = '2017/2018 PEA - Project Management';
+            masterTitle.innerHTML = 'Project Management';
+            break;
+        default:
+            masterTitle.innerHTML = capitalizeWord(filename);
+            masterDom.title = `2017/2018 PEA - ${capitalizeWord(filename)}`;
+            break;
+        }
+
         masterContent.innerHTML = Markdown.toHTML(htmlContent);
         Fs.writeFileSync(`../${filename}.html`, masterDom.documentElement.outerHTML, 'utf8');
     });
